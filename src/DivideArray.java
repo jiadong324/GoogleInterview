cae29c0093d495400aeeb0ca0da6d4ea25a9092f
jiadong324 <jdlin910324@gmail.com>
2016-04-13 10:51:11 -0400

    Changes to DivideArray class

--- src/DivideArray.java
+++ src/DivideArray.java
@@ -16,34 +16,10 @@ public class DivideArray {
 			return false;
 		}
 		Arrays.sort(input);
-		List<Integer> cur = new ArrayList<>();
-		helper(input,0,cur,res);
-		System.out.print(res.size());
-		for(List<Integer> list:res){
-			System.out.print(list.toString()+'\t');
-		}
+
 		
 		return true;
 		
-	}
-	public static void helper(int[] curNums,int pos,List<Integer> cur, List<List<Integer>> res){
-		if(cur.size() == 5){
-			res.add(new ArrayList<Integer>(cur));
-			return;
-		}
-		if(cur.size()== 0){
-			cur.add(curNums[pos]);
-		}
-		
-		for(int i=pos+1;i<curNums.length;i++){
-			
-			if(cur.get(cur.size()-1)+1 == curNums[i]){
-				cur.add(curNums[i]);
-				helper(curNums,pos+1,cur,res);
-				cur.remove(cur.size()-1);
-			}
-			
-		}
 	}
 	
 	
